0cdafa8ab1937f98dc1165cde0a2f5dd6056b1c3
ruan65 <ruan65@gmail.com>
2018-01-19 18:06:59 +0300

    autodeploy is working

--- src/main/java/ruCh/messenger/rea/controllers/GitController.java
+++ src/main/java/ruCh/messenger/rea/controllers/GitController.java
@@ -20,12 +20,11 @@ import java.util.Date;
 @RestController
 public class GitController {
 
-    static String fileName = "/home/a/temp/writtenByJava.txt";
+//    static String fileName = "/home/a/temp/writtenByJava.txt";
+    static String fileName = "/home/a/dev/reaproto/reaproto_2/reaproto/ruch_mobile_chat_client/app/src/main/res/raw/text.txt";
 
     @RequestMapping(value = "/change/text", method = RequestMethod.POST)
     public OutputMessage changeText(@RequestBody InputMessage msg) {
-
-        System.out.println("Writing to file " + fileName);
 
         writeToFileHelper(fileName, msg.getText());
 
@@ -44,6 +43,24 @@ public class GitController {
         return text;
     }
 
+    @RequestMapping("deploy/apk")
+    public OutputMessage deployApk() {
+
+        System.out.println("Deploy apk called");
+        deploy();
+        notifyUbuntu();
+        return createDeployOutput();
+    }
+
+
+
+
+
+
+    public static OutputMessage createDeployOutput() {
+        return new OutputMessage("Redsys deployment tool", "Deployed, Run on the device now!", new Date().toString());
+    }
+
     public static void writeToFileHelper(String fileName, String text) {
 
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(fileName))) {
@@ -52,6 +69,27 @@ public class GitController {
 
             System.out.println("Done!!!");
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void deploy() {
+
+        String[] cmd = new String[]{"/bin/sh", "/home/a/dev/reaproto/reaproto_2/reaproto/ruch_mobile_chat_client/deployApk.sh > /home/a/dev/reaproto/reaproto_2/reaproto/ruch_mobile_chat_client/out.log"};
+        try {
+            Process pr = Runtime.getRuntime().exec(cmd);
+            System.out.println("Deployed!!!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void notifyUbuntu() {
+        String[] cmd = new String[]{"/bin/sh", "/home/a/dev/reaproto/reaproto_2/reaproto/ruch_mobile_chat_client/notify2.sh"};
+        try {
+            Process pr = Runtime.getRuntime().exec(cmd);
+//            pr.getOutputStream()
         } catch (IOException e) {
             e.printStackTrace();
         }
